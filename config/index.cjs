c0d1be963618a6929ba3595eaa3a4f57cf64b0bd
MYBGeek <34680559+MYBGeek@users.noreply.github.com>
2024-03-13 11:28:08 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oY-O_6ceErcxTlFHG2ZWqrf75RlM',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '_BNMLl8fMuaAVn9QqB8ikCBvo2oxRoIMOYfpCdH8-74	',
+      useTemplateId: '_BNMLl8fMuaAVn9QqB8ikCBvo2oxRoIMOYfpCdH8-74',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '08-18',
       festivals: [
